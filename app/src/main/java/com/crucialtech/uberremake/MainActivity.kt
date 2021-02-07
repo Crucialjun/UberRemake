5bf08fce473c602ba8c8dd8d95a238cddc26d6da
Crucialjun <nich.otieno@gmail.com>
2021-02-07 16:07:03 +0300

    Phone authenticator

--- app/src/main/java/com/crucialtech/uberremake/MainActivity.kt
+++ app/src/main/java/com/crucialtech/uberremake/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.crucialtech.uberremake
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.app.Notification
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -8,6 +9,7 @@ import android.os.Bundle
 import android.widget.Toast
 import com.firebase.ui.auth.AuthMethodPickerLayout
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 import io.reactivex.Completable
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -63,12 +65,27 @@ class MainActivity : AppCompatActivity() {
             AuthUI.getInstance()
                 .createSignInIntentBuilder()
                 .setAuthMethodPickerLayout(authMethodPickerLayout)
-                .setTheme(R.style.LoginTheme)
+                //.setTheme(R.style.LoginTheme)
                 .setAvailableProviders(providers)
                 .setIsSmartLockEnabled(false)
                 .build(),
             LOGIN_REQUEST_CODE
         )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == LOGIN_REQUEST_CODE){
+            val response = IdpResponse.fromResultIntent(data)
+            if(resultCode == Activity.RESULT_OK){
+                val user = FirebaseAuth.getInstance().currentUser
+                
+            }
+            else{
+                Toast.makeText(this, "${response?.error?.message}", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     override fun onStop() {
