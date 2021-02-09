decb5a00f60f53a834cd407ee0ccf422d165c4a5
Crucialjun <nich.otieno@gmail.com>
2021-02-09 20:00:39 +0300

    Add Navigation details

--- app/src/main/java/com/crucialtech/uberremake/Common.kt
+++ app/src/main/java/com/crucialtech/uberremake/Common.kt
@@ -1,5 +1,12 @@
 package com.crucialtech.uberremake
 
+import java.lang.StringBuilder
+
 object Common {
+
+    fun buildWelcomeMessage():String{
+        return "Welcome ${currentUser?.firstName} ${currentUser?.lastName} "
+    }
+
     var currentUser : DriverInfoModel? = null
 }
--- app/src/main/java/com/crucialtech/uberremake/DriverHomeActivity.kt
+++ app/src/main/java/com/crucialtech/uberremake/DriverHomeActivity.kt
@@ -1,7 +1,10 @@
 package com.crucialtech.uberremake
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
@@ -13,10 +16,19 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import com.google.firebase.auth.FirebaseAuth
 
 class DriverHomeActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+
+
+    lateinit var navView: NavigationView
+
+    lateinit var drawerLayout: DrawerLayout
+
+    lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,18 +37,63 @@ class DriverHomeActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
 
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        val navView: NavigationView = findViewById(R.id.nav_view)
-        val navController = findNavController(R.id.nav_host_fragment)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navView = findViewById(R.id.nav_view)
+        navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_home
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        init()
+    }
+
+    private fun init() {
+        navView.setNavigationItemSelectedListener{
+            if(it.itemId == R.id.nav_signout){
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("Sign out")
+                    .setMessage("Do you really want to sign out")
+                    .setNegativeButton("CANCEL") { dialogInterface, i ->
+                        dialogInterface.dismiss()
+
+                    }
+                    .setPositiveButton("SIGN OUT") { _, _ ->
+                        FirebaseAuth.getInstance().signOut()
+                        val intent = Intent(this, MainActivity::class.java)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                        startActivity(intent)
+                        finish()
+                    }
+                    .setCancelable(false)
+
+                val dialog = builder.create()
+                dialog.setOnShowListener {
+                    dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+                        .setTextColor(resources.getColor(android.R.color.holo_red_dark))
+                    dialog.getButton(AlertDialog.BUTTON_NEGATIVE)
+                        .setTextColor(resources.getColor(R.color.colorAccent))
+                }
+
+                dialog.show()
+            }
+            true
+        }
+
+
+        val headerView = navView.getHeaderView(0)
+        val txt_name = headerView.findViewById<TextView>(R.id.txt_name)
+        val txt_photo = headerView.findViewById<TextView>(R.id.txt_phone)
+        val txt_star = headerView.findViewById<TextView>(R.id.txt_star)
+
+        txt_name.text = Common.buildWelcomeMessage()
+        txt_photo.text = Common.currentUser?.phoneNumber
+        txt_star.text = Common.currentUser?.rating.toString()
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
